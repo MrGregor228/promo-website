5b8233ed0d55cbe534ab905220abf95aa9829222
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2021-04-02 15:17:55 +0300

    Update

--- js/js.js
+++ js/js.js
@@ -1,3 +1,6 @@
+let scroll = new SmoothScroll('a[href*="#"]', {
+    offset: 100
+});
 let videoFrame = document.querySelector('iframe#video-iframe');
 
 window.addEventListener('resize', function () {
@@ -6,3 +9,36 @@ window.addEventListener('resize', function () {
 window.addEventListener('load', function () {
     videoFrame.style.height = document.querySelector("iframe").contentDocument.querySelector("body > video").clientHeight + "px";
 });
+
+const menuToggler = document.querySelector('.openMenu'),
+	  menu = document.querySelector('.navigation-menu'),
+	  menuCloser = document.querySelector('.close'),
+      navigationMenuLink = document.querySelectorAll('.navigation-menu__link'),
+      catalogModal = document.querySelector('.submenu-catalog'),
+      catalogButton = document.getElementById('catalog-button');
+
+menuToggler.addEventListener('click', () => {
+	menu.classList.add('opened');
+	menu.classList.remove('closed');
+});
+
+menuCloser.addEventListener('click', () => {
+	menu.classList.add('closed');
+	setTimeout(() => {menu.classList.remove('opened');},500);
+	console.log(menuCloser);
+});
+
+navigationMenuLink.forEach(link => {
+    if (link.childNodes[0].tagName == 'BUTTON') {
+        catalogButton.addEventListener('click', () => {
+            catalogModal.classList.toggle('opened');
+        });
+    } else {
+        link.addEventListener('click', () => {
+            menu.classList.add('closed');
+            setTimeout(() => {menu.classList.remove('opened');},500);
+        });
+    }
+    
+});
+
